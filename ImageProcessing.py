051174d1c9a9b612741d1f281dee01c83d814dcd
ninamoutonnet <nm2318@ic.ac.uk>
2022-04-19 15:46:30 +0200

    added a button for pixel 1D variation with no signal processing

--- ImageProcessing.py
+++ ImageProcessing.py
@@ -4,79 +4,104 @@ from PIL import ImageSequence
 from PIL import TiffImagePlugin
 import numpy as np
 import os
+import matplotlib
+import hyperspy.api as hs
 # Importing the statistics module
 import statistics
 from matplotlib import pyplot as plt
 import tifffile
 
+
 # # # # # # #
 #  source   #  https://stackoverflow.com/questions/50702024/multi-page-tiff-resizing-python
 # # # # # # #
-
 
 def fluoMap(filename):
     # # # # # # #
     #  source   # https://stackoverflow.com/questions/50702024/multi-page-tiff-resizing-python
     # # # # # # #
 
-    os.remove('multipage_tif_resized.tif')
+    try:
+        os.remove('multipage_tif_resized.tif')
+    except:
+        print("An exception occurred")
 
     INFILE = filename
     RESIZED_STACK = 'multipage_tif_resized.tif'
     OUTFILE2 = 'average_tif_resized.tif'
 
-
     # print('Resizing TIF pages')
-    '''pages = []
+    pages = []
     imagehandler = Image.open(INFILE)
     for page in ImageSequence.Iterator(imagehandler):
-        new_size = (int(page.size[0]/8), int(page.size[1]/8))
+        new_size = (int(page.size[0] / 4), int(page.size[1] / 4))
         page = page.resize(new_size)
         pages.append(page)
-    print('Writing multipage TIF: ', pages[0])
+    #print('Writing multipage TIF: ', pages[0])
     with TiffImagePlugin.AppendingTiffWriter(RESIZED_STACK) as tf:
         for page in pages:
             page.save(tf)
-            tf.newFrame()'''
+            tf.newFrame()
 
-    infile = tifffile.imread(INFILE)
+    '''infile = tifffile.imread(INFILE)
     print('shape is: ', infile.shape)
-    infile.resize(int(infile.shape[1]), int(infile.shape[2]))
+    infile.resize(200, 256, 256)
     plt.gray()
     plt.imshow(infile)
     plt.show()
-    print(infile[0, 0, 0])
+    print(infile[0, 0, 0])'''
 
     # Now that you have resized the whole stack, start the processing
     # Read the image from the TIFF file as numpy array:
     imfile = tifffile.imread(RESIZED_STACK)
-    print(imfile[0,0,0])
+    # image_array = imfile.astype('uint8')
+    # print('shape of 8 bit is: ', image_array.shape)
+    # plt.gray()
+    # plt.imshow(image_array[0])
+    # plt.show()
+
+    # print(imfile.dtype, image_array.dtype)
+    # print('max: ', np.amax(imfile), ' ', np.amax(image_array))
+    # print('min: ', np.amin(imfile), ' ', np.amin(image_array))
 
     # Take the mean, pixel per pixel of the whole image
     OUTFILE2_NP = imfile.mean(axis=0)
-    print(OUTFILE2_NP[0,0])
-    #plt.gray()
+    plt.gray()
     #plt.imshow(OUTFILE2_NP)
     #plt.show()
 
     # Create the np array that will be the map
-    heat_map_np_array = imfile
+    heat_map_np_array = np.copy(imfile)
+    # print(heat_map_np_array[1])
+    # print(OUTFILE2_NP)
+    # print((heat_map_np_array[1]-OUTFILE2_NP) / OUTFILE2_NP)
+    # print(np.amax((heat_map_np_array[1]-OUTFILE2_NP) / OUTFILE2_NP))
+    # print(np.amin((heat_map_np_array[1] - OUTFILE2_NP) / OUTFILE2_NP))
+
     for i in range(imfile.shape[0]):
-        heat_map_np_array[i] = (heat_map_np_array[i]/OUTFILE2_NP)*255
+        temp = (imfile[i] - OUTFILE2_NP) / OUTFILE2_NP
+        min = np.amin(temp)
+        max = np.amax(temp)
+        scaling_factor = (max - min) / 65535
+        print(min, '   ', max, '   ', scaling_factor, np.amin((temp - min)) / scaling_factor, ' ',
+              np.amax((temp - min)) / scaling_factor)
+        # heat_map_np_array[i] = (temp-min)/scaling_factor
+        heat_map_np_array[i] = abs(imfile[i] - OUTFILE2_NP)
 
     tifffile.imwrite(OUTFILE2, heat_map_np_array, photometric='minisblack')
+    # print('size of fluo output: ', heat_map_np_array.shape)
+
     return OUTFILE2
 
-    ''''# Read the tiff stack
-    # this reads the tiff file as a gray scale image
-    ret, images = cv2.imreadmulti(filename, [], cv2.IMREAD_GRAYSCALE)
-    imgArr = images[0]  # note 0 based indexing, this is the 1st image of the stiff
-
-    # compress the image to a 256 by 256 image
-    img = Image.fromarray(imgArr)
-    new_image = img.resize((256, 256))
-    # convert it into an array
-    np_array_low_resolution_img = numpy.array(new_image)'''
 
 
+def colourMap(filename):
+    try:
+        os.remove('multipage_tif_resized_COLOUR_MAP.tif')
+    except:
+        print("An exception occurred")
 
+    OUTFILE = "multipage_tif_resized_COLOUR_MAP.tif"
+    imfile = tifffile.imread('average_tif_resized.tif')
+
+    return
--- final.py
+++ final.py
@@ -3,55 +3,73 @@ from PyQt5 import QtWidgets as qtw
 from PyQt5 import QtGui as qtg
 from PyQt5 import QtCore as qtc
 from MultiPageTIFFViewerQt import MultiPageTIFFViewerQt
+import pyqtgraph as pg
 from ImageProcessing import fluoMap
+import tifffile
 import os
 
-class fluorescenceIntensityMap(qtw.QWidget):
 
-    def __init__(self, filename):
-        '''Main Window constructor'''
+class PixelTemporalVariation(qtw.QWidget):
+
+    def __init__(self, x, y, filename):
         super().__init__()
         #  start UI code
         # QLabel
-        label = qtw.QLabel(filename, self, margin=10)
+        label = qtw.QLabel('Pixel Variation 1D', self, margin=10)
 
         # give the tiff file to the intensity function
-        fluo_output = fluoMap(filename)
-        stackViewer = MultiPageTIFFViewerQt()
-        # As this next command has no argument, the files will pop up and the
-        # user will be asked to get the tiff stack to open
-        stackViewer.loadImageStackFromFile(fluo_output)
+        print('pixel x: ', x , 'and y: ', y, 'of file: ', filename)
+
+        # Read the image from the TIFF file as numpy array:
+        imfile = tifffile.imread(filename)
+        # print('shape: ', imfile.shape)
+        extracted = imfile[:, x, y]
+        # print('shape: ', extracted.shape)
+
+        graphWidget = pg.PlotWidget()
+        pen = pg.mkPen(color=(255, 0, 0), width=5)
+        graphWidget.setBackground('w')
+        graphWidget.plot(extracted, pen=pen)
+        graphWidget.showGrid(x=True, y=True)
+        graphWidget.setTitle("Temporal Trace of ("+str(x)+","+str(y)+")", size="30pt")
+        styles = {'color': 'b', 'font-size': '20px'}
+        graphWidget.setLabel('left', 'Intensity normalised by the average intensity', **styles)
+        graphWidget.setLabel('bottom', 'Frame number', **styles)
 
 
         # Add widget objects to a layout
         layout = qtw.QVBoxLayout()
         self.setLayout(layout)
-        layout.addWidget(stackViewer)
         layout.addWidget(label)
+        layout.addWidget(graphWidget)
 
         #  end main UI code - Display the UI
         self.show()
 
 
-class MainWindow(qtw.QWidget):
+class FluorescenceIntensityMap(qtw.QWidget):
 
-    def __init__(self):
+    def __init__(self, filename):
         '''Main Window constructor'''
         super().__init__()
+        #  get the filname/path of the original tiff stack to use when looking at individual pixel intensity
+        self.filename = filename
 
-        # main UI code goes here
-        # Create an image stack viewer widget.
-        stackViewer = MultiPageTIFFViewerQt()
+        #  start UI code
+        # QLabel
+        label = qtw.QLabel(filename, self, margin=10)
+
+        # give the tiff file to the intensity function
+        self.fluo_output = fluoMap(filename)
+        self.stackViewer = MultiPageTIFFViewerQt()
         # As this next command has no argument, the files will pop up and the
         # user will be asked to get the tiff stack to open
-        stackViewer.loadImageStackFromFile()
-        # the file path is stored in filename
-        self.filename = stackViewer.openFileName
+        self.stackViewer.loadImageStackFromFile(self.fluo_output)
 
-        # Create a button to see the varying intensity.
+        # Create a button to see 1D intensity variation
         # QPushButton
         button = qtw.QPushButton(
-            "Fluorescence variation Map",
+            "Normalised pixel temporal intensity variation",
             self,
             checkable=True,
             checked=True,
@@ -61,27 +79,105 @@ class MainWindow(qtw.QWidget):
         # Add widget objects to a layout
         layout = qtw.QVBoxLayout()
         self.setLayout(layout)
-        layout.addWidget(stackViewer)
+        layout.addWidget(self.stackViewer)
+        layout.addWidget(label)
         layout.addWidget(button)
 
         #  end main UI code - Display the UI
         self.show()
 
         #  If the button is pressed, open a new window
-        button.clicked.connect(self.windowFluo)
+        button.clicked.connect(self.WindowIntensityVariation)
 
+    def GetPos(self, event):
+        # get the x pixel correction
+        x = event.pos().x()
+        y = event.pos().y()
+        print('x= ', x)
+        print('y= ', y)
+        self.w = PixelTemporalVariation(x, y, self.fluo_output) # self.filename)
+        self.w.show()
+
+    def WindowIntensityVariation(self):
+        # Add event when the buttons are pressed -> get the position of interest
+        self.stackViewer.viewer.mousePressEvent = self.GetPos
+
+
+class MainWindow(qtw.QWidget):
+
+    def __init__(self):
+        '''Main Window constructor'''
+        super().__init__()
+
+        # main UI code goes here
+
+        # Create an image stack viewer widget.
+        self.stackViewer = MultiPageTIFFViewerQt()
+
+        # As this next command has no argument, the files will pop up and the user will be asked to get the tiff stack to open
+        self.stackViewer.loadImageStackFromFile()
+
+        # the file path is stored in filename
+        self.filename = self.stackViewer.openFileName
+
+        # Create a button to see the varying intensity: QPushButton
+        button = qtw.QPushButton(
+            "Fluorescence variation Map",
+            self,
+            checkable=True,
+            checked=True,
+            shortcut=qtg.QKeySequence('Ctrl+p')
+        )
+
+        # Create a button to see 1D intensity variation: QPushButton
+        button_pixel_intensity_var = qtw.QPushButton(
+            "Pixel temporal intensity variation",
+            self,
+            checkable=True,
+            checked=True,
+            shortcut=qtg.QKeySequence('Ctrl+p')
+        )
+
+        # Add widget objects to a layout
+        layout = qtw.QVBoxLayout()
+        self.setLayout(layout)
+        layout.addWidget(self.stackViewer)
+        layout.addWidget(button)
+        layout.addWidget(button_pixel_intensity_var)
+
+        #  end main UI code - Display the UI
+        self.show()
+
+        #  If the button for fluorescence is pressed, open a new window
+        button.clicked.connect(self.windowFluo)
+        #  If the button is pressed, open a new window
+        button_pixel_intensity_var.clicked.connect(self.WindowIntensityVariation)
+
+    def GetPos(self, event):
+        # get the viewer pixel size
+        viewer_size = self.stackViewer.viewer.size()
+        print('viewer size ', self.stackViewer.viewer.size())
+        # get the image size CHANGE THIS! THIS IS HARDCODED
+        image_size = (512, 512)
+        # get the differences in x/y coordinates -> they are divided by 2 because the image is centred in the viewer.
+        # subtract that when using the x/y coordinates
+
+        x = event.pos().x()
+        y = event.pos().y()
+        print('x= ', x)
+        print('y= ', y)
+        self.w = PixelTemporalVariation(x, y, self.filename)
+        self.w.show()
+
+    def WindowIntensityVariation(self):
+        # Add event when the buttons are pressed -> get the position of interest
+        self.stackViewer.viewer.mousePressEvent = self.GetPos
 
     def windowFluo(self):
-        self.w = fluorescenceIntensityMap(self.filename)
+        self.w = FluorescenceIntensityMap(self.filename)
+
 
 if __name__ == '__main__':
     app = qtw.QApplication(sys.argv)
     mw = MainWindow()
     sys.argv(app.exec())
-
-
-
-
-
-
-
